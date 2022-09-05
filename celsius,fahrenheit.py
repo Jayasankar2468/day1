5cf10e8c990f79bba72567367ab834abd3c9c955
Jayasankar2468 <112851253+Jayasankar2468@users.noreply.github.com>
2022-09-05 13:36:37 +0530

    Add files via upload

---
+++ celsius,fahrenheit.py
@@ -0,0 +1,6 @@
+#python program to convert temperature in celsius to fahrenheit
+#change this value for a different result
+celsius=60.6
+#calculate fahrenheit
+fahrenheit=(celsius*1.8)+60.6
+print('%0.1f degree celsius is equal to %0.1f degree fahrenhit'%(celsius,fahrenheit))
